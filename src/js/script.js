6796166dcde8384ed0abaaea5d1cc3a99bcc6156
Mohmedalfin <mohmedalfin170505@gmail.com>
2025-07-24 16:38:08 +0700

    update alfin

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,7 @@
 const hamburger = document.querySelector('#hamburger');
 const navMenu = document.querySelector('#nav-menu');
 
+
 hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
@@ -11,10 +12,33 @@ hamburger.addEventListener('click', function() {
 window.onscroll = function() {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
 
     if(window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
     } else {
-        header.classList.remove('navbar-fixed');    
+        header.classList.remove('navbar-fixed');
+        toTop.classList.add('hidden');
     }
 }
+
+// Klik diluar hamburger untuk menutup menu
+window.addEventListener('click', function(e){
+    if(e.target != navMenu && e.target != hamburger) {
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
+})
+
+// Dark mode toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function() {
+   if(darkToggle.checked){
+    html.classList.add('dark');
+   }else{
+    html.classList.remove('dark');
+   }
+})
